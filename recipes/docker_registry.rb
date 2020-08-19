f3952953b2c5720837db992ddc5806bb168d314c
Robin Andersson <robin.eric.andersson@gmail.com>
2020-08-19 15:50:03 +0200

    fix

--- recipes/docker_registry.rb
+++ recipes/docker_registry.rb
@@ -132,8 +132,8 @@ end
 bash "tag_images" do
   user "root"
   code <<-EOF
-    docker tag #{node['hops']['docker']['base']['image']['name']} #{base_image}
-    docker tag #{node['hops']['docker']['base']['image']['python']['name']} #{base_image_python}
+    docker tag #{node['hops']['docker']['base']['image']['name']}:#{node['hops']['docker_img_version']} #{base_image}
+    docker tag #{node['hops']['docker']['base']['image']['python']['name']}:#{node['hops']['docker_img_version']} #{base_image_python}
   EOF
   not_if "docker image inspect #{base_image} #{base_image_python}"
 end
